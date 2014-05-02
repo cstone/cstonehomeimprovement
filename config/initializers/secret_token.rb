e7333fbaadb1914ff0f86f9a0ae69412facffb20
Cavin <cstone2003@hotmail.com>
2014-05-02 17:38:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cstonehomeimprovement::Application.config.secret_token = '962bc0cadf84698d452cc53f64b431635879235adb87cff359fece2f47194f08511f42b6b092804db26f470745b2d0e2c8d6c96b5d3535bc3d278dc3dcad3f6d'
