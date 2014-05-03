206955546715e887f0ce516bfd7233d21e0b47b7
Cavin <cstone2003@hotmail.com>
2014-05-03 07:58:39 -0500

    Add Dynamic Content model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140503030558) do
+ActiveRecord::Schema.define(:version => 20140503125632) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(:version => 20140503030558) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "dynamic_contents", :force => true do |t|
+    t.string   "title"
+    t.string   "key"
+    t.text     "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pages", :force => true do |t|
     t.string   "title"
