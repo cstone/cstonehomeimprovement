ff0bc2f45cd5c1eb436334ab8b210199af9c502d
Cavin <cstone2003@hotmail.com>
2014-05-03 00:36:59 -0500

    Add Project Image Uploader to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :description, :project_image, :title
+  attr_accessible :description, :project_image, :title, :remove_project_image, :project_image_cache
+
+  mount_uploader :project_image, ProjectImageUploader
 end
