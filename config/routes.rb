2b169d451c15a86bc1833fa984c3410195ad2b59
Cavin <cstone2003@hotmail.com>
2014-05-02 22:10:10 -0500

    Add Project scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Cstonehomeimprovement::Application.routes.draw do
+  resources :projects
+
+
   resources :testimonials
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140503021516) do
+ActiveRecord::Schema.define(:version => 20140503030558) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(:version => 20140503021516) do
     t.string   "page_image"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "project_image"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "testimonials", :force => true do |t|
