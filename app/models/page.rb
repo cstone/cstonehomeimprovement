16dcfdc8001164082d8eb324f064cc13f5dff7d0
Cavin <cstone2003@hotmail.com>
2014-05-02 20:06:47 -0500

    Add Page scaffold

---
+++ app/models/page.rb
@@ -0,0 +1,11 @@
+class Page < ActiveRecord::Base
+  attr_accessible :content, :page_image, :permalink, :title#, :remove_page_image, :page_image_cache
+
+  validates_uniqueness_of :permalink
+
+  #mount_uploader :page_image, PageImageUploader
+
+  def to_param
+    permalink
+  end
+end
