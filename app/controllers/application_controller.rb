0c754717c41c1f5fb505a566b6d5089387bdd05e
Cavin <cstone2003@hotmail.com>
2014-05-03 08:08:36 -0500

    Add get_dynamic_content method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_dynamic_content
+
+  def get_dynamic_content
+    @site_title = DynamicContent.get_value(:site_title)
+    @home_title = DynamicContent.get_value(:home_title)
+    @home_content = DynamicContent.get_value(:home_content)
+    @contact_name = DynamicContent.get_value(:contact_name)
+    @contact_content = DynamicContent.get_value(:contact_content)
+  end
 end
